8acff3e6c6e44d492c94033b2ec42d6454551a9f
Xavier Sans <xavi.sans.b@gmail.com>
2023-03-05 22:22:33 +0100

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text} from 'react-native';
+
+const App = () => {
+  return <Text>This is my first try at Native</Text>;
+};
+
+export default App;
